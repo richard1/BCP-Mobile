244fd90839e4dc3b9ae73e39b370a35ef7859209
Jonathan Chang <jonathan.chang13@gmail.com>
2013-05-07 13:12:29 -0700

    Login form created

--- BCPGradebook/gen/bcp/web/bcpgradebook/R.java
+++ BCPGradebook/gen/bcp/web/bcpgradebook/R.java
@@ -14,18 +14,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070006;
+        public static final int loginname=0x7f070002;
+        public static final int loginpass=0x7f070005;
+        public static final int menu_settings=0x7f070007;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070003;
+        public static final int textView3=0x7f070004;
+        public static final int webView1=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_grade_view=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_grade_view=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int login=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int password=0x7f040005;
+        public static final int submit=0x7f040006;
+        public static final int title_activity_grade_view=0x7f040007;
+        public static final int username=0x7f040004;
     }
     public static final class style {
         /** 
